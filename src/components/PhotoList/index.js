4f263ed1bfa16854a21950ae0f682c9fdc3d0059
themadtaco <aatrox48@gmail.com>
2022-04-14 10:05:58 -0500

    finished test for modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
-//import Modal from '../Modal';
+import Modal from '../Modal';
 
 const PhotoList = ({category}) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const [photos] = useState([
         {
           name: 'Grocery aisle',
@@ -100,16 +103,25 @@ const PhotoList = ({category}) => {
         },
       ]);
 
-      const currentPhotos = photos.filter((photo) => photo.category === category);
+    const currentPhotos = photos.filter((photo) => photo.category === category);
+
+    const [currentPhoto, setCurrentPhotos] = useState();
+    
+    const toggleModal = (image, i) => {
+      setCurrentPhotos({...image, index: i});
+      setIsModalOpen(!isModalOpen);
+    }
 
     return (
         <div>
+          {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>}
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img
                     src={require(`../../assets/small/${category}/${i}.jpg`)}
                     alt={image.name}
                     className="img-thumbnail mx-1"
+                    onClick={() => toggleModal(image, i)}
                     key={image.name}
                     />
                 ))}
